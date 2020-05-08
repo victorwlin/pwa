b65bae7506aa5e054a0c50d332c771e520409b3b
Victor <57220247+victorwlin@users.noreply.github.com>
2020-05-08 12:20:34 +0800

    Initial

---
+++ docs/precache-manifest.5bc72d12b676ef67bf75195652967fb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde157c4564d4cdfa3ae404f4f21abf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5516cdc8b2b81e07106a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7dfaf560f2776abee08c",
+    "url": "/static/js/2.7f5feced.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f5feced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5516cdc8b2b81e07106a",
+    "url": "/static/js/main.ff4c309f.chunk.js"
+  },
+  {
+    "revision": "ebcd4b5c5528a41db26d",
+    "url": "/static/js/runtime-main.50dbde75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
